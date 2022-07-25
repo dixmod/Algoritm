2b93fe31e349b980094e5c1d7b140c118d35b740
dmipopov <dpopov@direct-credit.ru>
2022-07-25 17:09:33 +0300

    Add PreorderTraversal

--- PreorderTraversal/Example.php
+++ PreorderTraversal/Example.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace App\PreorderTraversal;
 
-use App\PreorderTraversal\Factory;
-
 require '../vendor/autoload.php';
 
 $root = Factory::create([1, null, 2, 3]);
-//print_r($root); exit;
+
 var_dump(
     (new Solution())->preorderTraversal($root)
 ); // 2
--- PreorderTraversal/Factory.php
+++ PreorderTraversal/Factory.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\PreorderTraversal;
 
-use App\PreorderTraversal\TreeNode;
-
 class Factory
 {
     private static array $arr;
