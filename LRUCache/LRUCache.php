1b0b4a6b0d8ff00a26756f6029efead15c15462c
dmipopov <dpopov@direct-credit.ru>
2022-09-15 17:20:26 +0300

    Add LRUCache

--- LRUCache/LRUCache.php
+++ LRUCache/LRUCache.php
@@ -9,7 +9,7 @@ class LRUCache
     private int $capacity;
     private array $cache = [];
     private array $queue = [];
-    private int $countPut = 0;
+    private int $sizeCache = 0;
 
     function __construct(int $capacity)
     {
@@ -22,36 +22,38 @@ class LRUCache
             return -1;
         }
 
-        $this->updateQueue($key);
+        $this->upKey($key);
 
         return $this->cache[$key];
     }
 
     function put(int $key, int $value): void
     {
-        if (true === $this->needClear($key)) {
-            unset($this->cache[array_shift($this->queue)]);
+        if (false === array_key_exists($key, $this->cache)) {
+            if($this->sizeCache >= $this->capacity){
+                $this->delOld();
+            }
+            ++$this->sizeCache;
         }
-
-        $this->updateQueue($key);
-        ++$this->countPut;
-
         $this->cache[$key] = $value;
+
+        $this->upKey($key);
     }
 
-    private function needClear(int $key): bool
-    {
-        return false === array_key_exists($key, $this->cache) && $this->countPut >= $this->capacity;
-    }
-
-    private function updateQueue(int $key): void
+    private function upKey(int $key): void
     {
         $this->queue = array_diff($this->queue, [$key]);
         $this->queue[] = $key;
     }
+
+    private function delOld(): void
+    {
+        unset($this->cache[array_shift($this->queue)]);
+        --$this->sizeCache;
+    }
 }
 
-/*$obj = new LRUCache(10);
+/*$obj = new LRUCache(2);
 $obj->put(2, 1);print_r($obj);
 $obj->put(1, 1);print_r($obj);
 $obj->put(2, 3);print_r($obj);
@@ -60,6 +62,113 @@ $obj->get(1); print_r($obj);
 $obj->get(2); print_r($obj);*/
 
 
-
-//[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
-
+$obj = new LRUCache(10);
+$obj->put(10, 13); print_r($obj);
+$obj->put(3, 17); print_r($obj);
+$obj->put(6, 11); print_r($obj);
+$obj->put(10, 5); print_r($obj);
+$obj->put(9, 10); print_r($obj);
+$obj->get(13); print_r($obj);
+$obj->put(2, 19); print_r($obj);
+$obj->get(2); print_r($obj);
+$obj->get(3); print_r($obj);
+$obj->put(5, 25); print_r($obj);
+$obj->get(8); print_r($obj);
+$obj->put(9, 22); print_r($obj);
+$obj->put(5, 5); print_r($obj);
+$obj->put(1, 30); print_r($obj); echo '---';///
+$obj->get(11); print_r($obj);
+$obj->put(9, 12); print_r($obj);
+$obj->get(7); print_r($obj);
+$obj->get(5); print_r($obj);
+$obj->get(8); print_r($obj);
+$obj->get(9); print_r($obj);
+$obj->put(4, 30); print_r($obj);
+$obj->put(9, 3); print_r($obj);
+$obj->get(9); print_r($obj);
+$obj->get(10); print_r($obj);
+$obj->get(10); print_r($obj);
+$obj->put(6, 14); print_r($obj);
+$obj->put(3, 1); print_r($obj);
+$obj->get(3); print_r($obj);
+$obj->put(10, 11); print_r($obj);
+$obj->get(8); print_r($obj);
+$obj->put(2, 14); print_r($obj);
+print_r($obj->get(1)); //print_r($obj); // 30
+//print_r($obj->get(5)); print_r($obj);
+//$obj->get(4); print_r($obj);
+//$obj->put(11, 4); print_r($obj);
+//$obj->put(12, 24); print_r($obj);
+//$obj->put(5, 18); print_r($obj);
+//$obj->get(13); print_r($obj);
+//$obj->put(7, 23); print_r($obj);
+//$obj->get(8); print_r($obj);
+//$obj->get(12); print_r($obj);
+//$obj->put(3, 27); print_r($obj);
+//$obj->put(2, 12); print_r($obj);
+//$obj->get(5); print_r($obj);
+//$obj->put(2, 9); print_r($obj);
+//$obj->put(13, 4); print_r($obj);
+//$obj->put(8, 18); print_r($obj);
+//$obj->put(1, 7); print_r($obj);
+//$obj->get(6); print_r($obj);
+//$obj->put(9, 29); print_r($obj);
+//$obj->put(8, 21); print_r($obj);
+//$obj->get(5); print_r($obj);
+//$obj->put(6, 30); print_r($obj);
+//$obj->put(1, 12); print_r($obj);
+//$obj->get(10); print_r($obj);
+//$obj->put(4, 15); print_r($obj);
+//$obj->put(7, 22); print_r($obj);
+//$obj->put(11, 26); print_r($obj);
+//$obj->put(8, 17); print_r($obj);
+//$obj->put(9, 29); print_r($obj);
+//$obj->get(5); print_r($obj);
+//$obj->put(3, 4); print_r($obj);
+//$obj->put(11, 30); print_r($obj);
+//$obj->get(12); print_r($obj);
+//$obj->put(4, 29); print_r($obj);
+//$obj->get(3); print_r($obj);
+//$obj->get(9); print_r($obj);
+//$obj->get(6); print_r($obj);
+//$obj->put(3, 4); print_r($obj);
+//$obj->get(1); print_r($obj);
+//$obj->get(10); print_r($obj);
+//$obj->put(3, 29); print_r($obj);
+//$obj->put(10, 28); print_r($obj);
+//$obj->put(1, 20); print_r($obj);
+//$obj->put(11, 13); print_r($obj);
+//$obj->get(3); print_r($obj);
+//$obj->put(3, 12); print_r($obj);
+//$obj->put(3, 8); print_r($obj);
+//$obj->put(10, 9); print_r($obj);
+//$obj->put(3, 26); print_r($obj);
+//$obj->get(8); print_r($obj);
+//$obj->get(7); print_r($obj);
+//$obj->get(5); print_r($obj);
+//$obj->put(13, 17); print_r($obj);
+//$obj->put(2, 27); print_r($obj);
+//$obj->put(11, 15); print_r($obj);
+//$obj->get(12); print_r($obj);
+//$obj->put(9, 19); print_r($obj);
+//$obj->put(2, 15); print_r($obj);
+//$obj->put(3, 16); print_r($obj);
+//$obj->get(1); print_r($obj);
+//$obj->put(12, 17); print_r($obj);
+//$obj->put(9, 1); print_r($obj);
+//$obj->put(6, 19); print_r($obj);
+//$obj->get(4); print_r($obj);
+//$obj->get(5); print_r($obj);
+//$obj->get(5); print_r($obj);
+//$obj->put(8, 1); print_r($obj);
+//$obj->put(11, 7); print_r($obj);
+//$obj->put(5, 2); print_r($obj);
+//$obj->put(9, 28); print_r($obj);
+//$obj->get(1); print_r($obj);
+//$obj->put(2, 2); print_r($obj);
+//$obj->put(7, 4); print_r($obj);
+//$obj->put(4, 22); print_r($obj);
+//$obj->put(7, 24); print_r($obj);
+//$obj->put(9, 26); print_r($obj);
+//$obj->put(13, 28); print_r($obj);
+//$obj->put(11, 26); print_r($obj);
