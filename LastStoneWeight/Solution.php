d935f759264f021c9c2b97d9fab8b793b943c45e
dmipopov <dpopov@direct-credit.ru>
2022-10-03 17:20:19 +0300

    Add LastStoneWeight

---
+++ LastStoneWeight/Solution.php
@@ -0,0 +1,18 @@
+<?php
+
+class Solution
+{
+    /**
+     * @param int[] $stones
+     */
+    function lastStoneWeight(array $stones): int
+    {
+        while (sizeof($stones) > 1) {
+            rsort($stones);
+
+            $stones[] = array_shift($stones) - array_shift($stones);
+        }
+
+        return $stones[0];
+    }
+}
