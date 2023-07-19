0feeee82b1b0447921b2e697a2eda422926329e7
dmipopov <dpopov@direct-credit.ru>
2023-07-19 23:48:43 +0300

    add new Solution

--- TrappingRainWater/Solution.php
+++ TrappingRainWater/Solution.php
@@ -9,62 +9,50 @@ namespace App\TrappingRainWater;
  */
 class Solution
 {
-    /** @var int[] */
-    protected array $heights;
-    protected int $countHeights;
-
     /**
      * @param int[] $heights
      * @return int
      */
     public function trap(array $heights): int
     {
+        $countHeights = count($heights) - 1;
         $capacity = 0;
-        $this->heights = $heights;
-        $this->countHeights = count($heights) - 1;
+        $leftLimit = [$heights[0]];
+        $rightLimit[$countHeights] = $heights[$countHeights];
 
-        for ($levelWater = 0; $levelWater < max($heights); $levelWater++) {
-            $picLeft = $this->getPicLeft($levelWater);
-            $picRight = $this->getPicRight($levelWater);
+        for ($leftIndex = 1; $leftIndex <= $countHeights; $leftIndex++) {
+            $leftLimit[$leftIndex] = $this->getHighLimit($leftLimit[$leftIndex - 1], $heights[$leftIndex]);
 
-            for ($index = $picLeft + 1; $index <= $picRight - 1; $index++) {
-                if ($heights[$index] - $levelWater <= 0) {
-                    ++$capacity;
-                }
-            }
+            $rightIndex = $countHeights - $leftIndex;
+            $rightLimit[$rightIndex] = $this->getHighLimit($rightLimit[$rightIndex + 1], $heights[$rightIndex]);
+        }
+
+        for ($index = 1; $index <= $countHeights; $index++) {
+            $capacity += $this->getLowLimit($leftLimit[$index], $rightLimit[$index]) - $heights[$index];
         }
 
         return $capacity;
     }
 
-    private function getPicRight(int $levelWater): int
+    private function getHighLimit(int $left, int $right): int
     {
-        for ($index = $this->countHeights; $index > 0; $index--) {
-            if ($this->isPic($index, $levelWater)) {
-                return $index;
-            }
+        if ($left > $right) {
+            return $left;
         }
 
-        return $this->countHeights;
+        return $right;
     }
 
-    private function getPicLeft(int $levelWater): int
+    private function getLowLimit(int $left, int $right): int
     {
-        for ($index = 0; $index <= $this->countHeights; $index++) {
-            if ($this->isPic($index, $levelWater)) {
-                return $index;
-            }
+        if ($left < $right) {
+            return $left;
         }
 
-        return 0;
-    }
-
-    private function isPic(int $index, int $levelWater): bool
-    {
-        return $this->heights[$index] - $levelWater > 0;
+        return $right;
     }
 }
 
 echo (new Solution())->trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
-echo PHP_EOL;
-echo (new Solution())->trap([4, 2, 0, 3, 2, 5]);
+//echo PHP_EOL;
+//echo (new Solution())->trap([4, 2, 0, 3, 2, 5]);
